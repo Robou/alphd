009d5423a0fb68fd120502f921f9f0fda9e88e63
Robou <romainbousson@gmail.com>
2025-10-15 20:00:37 +0200

    layout général page d'accueil, et bootstrap THREE ok

---
+++ src/app/models/page.tsx
@@ -0,0 +1,9 @@
+export default function ModelsList() {
+    return ( <div className="flex items-center justify-center min-h-screen">
+        <div className="text-center">
+            <p>Liste des modèles disponibles...</p>
+            <p>Page en construction</p>
+          
+        </div>
+      </div>)
+}
